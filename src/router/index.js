d2647e104d221ea339e42ab1c87016e3dabe6bb5
dong <lidongdong0216@outlook.com>
2019-11-16 00:12:54 +0800

    Vue-router报NavigationDuplicated错误解决

--- src/router/index.js
+++ src/router/index.js
@@ -55,3 +55,8 @@ router.beforeEach((to, from, next) => {
 })
 
 export default router
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
